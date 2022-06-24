d60a68024e1400d743b0fb5f4c4c3c13f70a8b04
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-06-24 21:17:38 +0530

    Made some minor changes

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -6,15 +6,15 @@ let to = 'Arya Stark';
 /*
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
-console.log(quote.indexOf('is'));
+let indexOfIs = quote.indexOf('is');
 /*
 2. Find the character at the index indexOfIs (Problem 1) in quote.
 */
-console.log(quote.charAt(6));
+quote.charAt(indexOfIs);
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
-console.log(`The index of first is in quote is ${quote.indexOf('is')}`);
+console.log(`The index of first is in quote is ${quote.charAt(indexOfIs)}`);
 /*
 4. Log the message for first 6 characters of quote like this.
   The character at index 0 is 'T'
@@ -24,46 +24,48 @@ console.log(`The index of first is in quote is ${quote.indexOf('is')}`);
   The character at index 4 is 'e'
   The character at index 5 is ' '
 */
-console.log(`The character at index 0 is '${quote.charAt(0)}', The character at index 1 is '${quote.charAt(1)}', The character at index 2 is '${quote.charAt(2)}', The character at index 3 is '${quote.charAt(3)}', The character at index 4 is '${quote.charAt(4)}', The character at index 5 is '${quote.charAt(5)}',The character at index 6 is '${quote.charAt(6)}'`);
+for (let i = 0; i < 6; i++) {
+  console.log(`The character at index ${i} is '${quote[i]}'`);
+}
 /*
 5. Using the variable from , to and quote variable dispaly this message
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
-console.log(`${from.concat(' ', 'said',' ', quote,' ', 'to', ' ', to)}`);
+console.log(`${from.concat( ' said ', quote, ' to ', to)}`);
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
 console.log(from.startsWith('rk'),
 to.startsWith('rk'),
-quote.startsWith('rk'));
+quote.startsWith('rk')); // console used only to log tha value
 /*
 7. Does quote includes the word "Only"
 */
-console.log(quote.includes('only'));
+console.log(quote.includes('only')); // console used only to log tha value
 /*
 8. Does quote includes the word " we"
 */
-console.log(quote.includes('we'));
+console.log(quote.includes('we')); // console used only to log tha value
 /*
 9. Find the index of the the word `we` in quote
 */
-console.log(quote.indexOf('we'));
+console.log(quote.indexOf('we')); // console used only to log tha value
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
 let quoteSplitted = quote.split(' ');
-console.log(quoteSplitted);
+console.log(quoteSplitted); // console used only to log tha value
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
 quoteSplitted.splice(quoteSplitted.length -1, 1, 'tomorrow');
-quoteSplitted.join('');
+quoteSplitted.join(' ');
 console.log(quoteSplitted);
+
 /*
 12. Find the index of second "o" in quote. Use indexOf
 */
-let indexOfFirst = quote.indexOf('o');
-console.log(quote.indexOf('o', indexOfFirst + 1));
+console.log(quote.indexOf('o', 10));
 /*
 13. Find the last index of letter "a" in quote.
 */
@@ -83,8 +85,8 @@ console.log(character70);
 /*
 16. Do same as (15) but the ... should come in start. Store the output in a new variable
 */
-let character15 = quote.padStart(15, '.');
-console.log(character15);
+let newStartcharacter70 = quote.padStart(70, '.');
+console.log(newStartcharacter70);
 /*
 17. Log the repeat of "Hello World!" 10 times.
 */
@@ -103,7 +105,7 @@ console.log(to);
 /*
 20. Make the quote of length 30 and put ... at the end. (use slice)
 */
-quote.slice(30).padEnd('.');
+quote.slice(0, 30) + '...';
 /*
 21. Find out does quote, from, to starts with "A"
 */
