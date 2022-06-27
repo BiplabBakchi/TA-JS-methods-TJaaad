d285bb18d73166e40e09c635961f7a7d6c3beae6
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-06-27 11:30:57 +0530

    Initial push

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -11,45 +11,60 @@ Clone the array before using sort method: [...arr]
 */
 
 // - Find the index of `101` in numbers
-
+numbers.indexOf(`101`);
 // - Find the last index of `9` in numbers
-
+numbers.indexOf(`9`);
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+strings.join(' ');
 // - Add two new words in the strings array "called" and "sentance"
-
+strings.push('called', 'sentance');
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+strings.join(' ');
 // - Remove the first word in the array (strings)
-
+strings.shift();
 // - Find all the words that contain 'is' use string method 'includes'
-
+strings.filter((strings) => {
+  return strings.includes('is');
+});
 // - Find all the words that contain 'is' use string method 'indexOf'
-
+strings.filter((strings) => {
+  return strings.indexOf('is');
+});
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
+numbers.every((num) => {
+  return num % 3 == 0;
+});
 // -  Sort Array from smallest to largest
+let numSorting = [...numbers].sort((a, b) => {
+  return a - b; 
+});
 
+let stringSorting = [...strings].sort();
 // - Remove the last word in strings
-
+strings.pop();
 // - Find largest number in numbers
-
+let largest = numbers.reduce((a, b) => {
+  return Math.max(a, b);
+}, -Infinity);
 // - Find longest string in strings
 
 // - Find all the even numbers
-
+numbers.filter((num) => num % 2 == 0);
 // - Find all the odd numbers
-
+numbers.filter((num) => num % 2 != 0);
 // - Place a new word at the start of the array use (unshift)
-
+strings.unshift('Hi');
 // - Make a subset of numbers array [18,9,7,11]
-
+let numbersSubset = numbers.slice(3, 7);
 // - Make a subset of strings array ['a','collection']
-
+let stringsSubset = strings.slice(2, 4);
 // - Replace 12 & 18 with 1221 and 1881
-
+numbers.indexOf(12);
+numbers[1] = 1221;
+numbers.indexOf(18);
+numbers[3] = 1881;
 // - Replace words in strings array with the length of the word
-
+strings[strings.length - 1] = 'text';
 // - Find the sum of the length of words using above question
 
 // - Customers Array
@@ -60,7 +75,7 @@ var customers = [
   { firstname: 'Jack', lastname: 'White' },
 ];
 // - Find all customers whose firstname starts with 'J'
-
+customers.filter()
 // - Create new array with only first name
 
 // - Create new array with all the full names (ex: "Joe Blogs")
